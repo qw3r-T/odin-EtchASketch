f5094ebaefb64d906b884766941636a18e9d468d
qw3r-T <qqw33r.T@gmail.com>
2022-07-12 15:48:18 +0200

    Create grid
    I created the logic to create the grid with a mix of CSS and JS. The
    size of an element is set in the JS with the
    containerWidth/amountOfElements

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+function createDiv(size) {
+    const div = document.createElement('div');
+    div.classList.add('gridElement');
+    div.style.width = `${size}px`;
+    div.style.width = `${size}px`;
+
+    return div;
+}
+
 function createGrid(size) {
     const container = document.getElementById('gridContainer');
     for (let i = 0; i < size**2; i++) {
-        const div = document.createElement('div');
-        div.classList.add('gridElement');
-        container.appendChild(div);
+        container.appendChild(createDiv(container.clientWidth / size));
     };
 }
+
+createGrid(16)
