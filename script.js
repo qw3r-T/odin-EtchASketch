e5faf24a2fee8542f5990a53ddefda5d8b23b740
qw3r-T <qqw33r.T@gmail.com>
2022-07-12 17:27:27 +0200

    hover effect

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ function createDiv(size) {
     div.classList.add('gridElement');
     div.style.width = `${size}px`;
     div.style.width = `${size}px`;
+    div.addEventListener('mouseover',(e) => e.target.style.backgroundColor = 'blue');
 
     return div;
-}
+};
 
 function createGrid(size) {
     const container = document.getElementById('gridContainer');
     for (let i = 0; i < size**2; i++) {
         container.appendChild(createDiv(container.clientWidth / size));
     };
-}
+};
 
 createGrid(16)
